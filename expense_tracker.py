d5b8d3ef174c4d6d8ded00bd1259b81e246e8200
Al-ameen <alameenabiola3@gmail.com>
2026-05-30 14:52:05 +0100

    create input data

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,37 +1,46 @@
 print("Hello, Welcome To SpendWise.")
 print("SpendWise is an all Expense Tracker that helps you track your spending habit.")
-print("To get started, Kindly")
+print("To get started, Kindly see below what i can help you with")
+
+print("1. Log an expense")
+print("2. View expense history")
 
 # add date function
-def add_date(date):
+def add_date():
     date = input("Enter Date: ")
     return date
 
 # add expense description
-def add_desc(exp_desc):
+def add_desc():
     exp_desc = input("your expenses was on what? ")
     return exp_desc
 
 # add amount
-def add_amt(amount):
-    amount = input("how much did you spend? ")
+def add_amt():
+    amount = float(input("how much did you spend? "))
     return amount
 
 # add category
-def add_cat(category):
+def add_cat():
     category = input("in what category does your expenses fall in to?\n(e.g. Housing, Food, Utility, Transport, Black Tax)")
     return category
 
 # add payment method
-def add_p_method(pay_method):
+def add_p_method():
     pay_method = input("how did you pay?\n(e.g. Cash, Debit Card, Credit Card)")
     return pay_method
 
 
 
+def log_expense ():
+    expense = {}
+    expense['date'] = add_date()
+    expense['description'] = add_desc()
+    expense['amount'] = add_amt()
+    expense['category'] = add_cat()
+    expense['payment_method'] = add_p_method()
 
+    return expense
+print(log_expense())
 
-
-
-print(add_date('date'))
 print("Thanks for the info provided. \nWe are keeping track")
